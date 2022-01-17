425f7dfbe2043cd4af76a7e73f10e4f62ace9ead
Рауан <ibragimov020302@gmail.com>
2022-01-17 21:59:43 +0300

    Bean scope Singleton and Prototype

--- src/main/java/ru/spring/TestSpring.java
+++ src/main/java/ru/spring/TestSpring.java
@@ -8,13 +8,22 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer1",MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayerList",MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        System.out.println(comparison);
+        firstMusicPlayer.playMusic();
+
+        System.out.println(firstMusicPlayer.getName());
+        System.out.println(firstMusicPlayer.getVolume());
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
 
         musicPlayer2.playMusic();
 
