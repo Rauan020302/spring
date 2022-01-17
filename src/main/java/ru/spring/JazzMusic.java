3cabf98400bcd7cc0b672899f9a06706d461f11e
Рауан <ibragimov020302@gmail.com>
2022-01-17 19:06:58 +0300

    SecondLesson Inversion of Control(IoC)

---
+++ src/main/java/ru/spring/JazzMusic.java
@@ -0,0 +1,8 @@
+package ru.spring;
+
+public class JazzMusic implements Music{
+    @Override
+    public String getSong() {
+        return "It's Relaxing music for you, my baby";
+    }
+}
--- src/main/java/ru/spring/TestSpring.java
+++ src/main/java/ru/spring/TestSpring.java
@@ -7,9 +7,12 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        TestBean testBean = context.getBean("testBean",TestBean.class);
 
-        System.out.println(testBean.getName());
+        Music music = context.getBean("music",Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
 
         context.close();
     }
