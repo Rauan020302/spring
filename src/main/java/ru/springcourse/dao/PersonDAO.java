abf692a4094f937141f576baa7c306d07a2895ef
Рауан <ibragimov020302@gmail.com>
2022-01-21 18:56:17 +0300

    Annotation ModelAttribute HTML forms (Thymeleaf)

--- src/main/java/ru/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -7,14 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public class PersonDAO {
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
-        people.add(new Person(1,"Alex"));
-        people.add(new Person(2,"Bob"));
-        people.add(new Person(3,"Tom"));
-        people.add(new Person(4,"Sam"));
+        people.add(new Person(++PEOPLE_COUNT,"Alex"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Sam"));
     }
     public List<Person> index() {
         return people;
@@ -22,5 +23,10 @@ public class PersonDAO {
 
     public Person show (int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
