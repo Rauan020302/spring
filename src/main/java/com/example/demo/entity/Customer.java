823a0b018222d173b22a7bcd502eac01c9d35bfd
Maksut Rauan <Ibragimov02@Gmail.com>
2022-04-27 21:25:00 +0300

    spring batch

---
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CUSTOMERS_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "contact")
+    private String contactNo;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "dob")
+    private String dob;
+}
